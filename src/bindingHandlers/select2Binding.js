28531ebea64fb8821a23a90e6b0ef286228416d1
paul <weByte>
2017-12-01 17:30:58 +0100

    + select2 integration

--- src/bindingHandlers/select2Binding.js
+++ src/bindingHandlers/select2Binding.js
@@ -7,22 +7,48 @@
  */
 
 define([
-    "knockout"
-], function (ko) {
+    "knockout",
+    "lodash",
+    "select2/select2",
+    "select2/select2/css/select2.css!css",
+], function (ko, _) {
 
     if( ko.bindingHandlers.select2 == undefined ) {
 
         ko.bindingHandlers.select2 = {
-            after: ["options", "value"],
+
             init: function (el, valueAccessor, allBindingsAccessor, viewModel) {
-                $(el).select2(ko.unwrap(valueAccessor()));
+                var bindingData = ko.unwrap(valueAccessor());
+
+                /**
+                 * prepare an data object for select2
+                 * (expects dedicated format: https://select2.org/data-sources/formats)
+                 */
+                var prepareData = function () {
+                    var r = [];
+                    _.forEach( ko.utils.unwrapObservable(bindingData.options), function ( obj ) {
+                        var idProp = bindingData.optionsValue ? bindingData.optionsValue : bindingData.optionsText;
+
+                        var tmpItem = {
+                            id: obj[idProp],
+                            text: obj[bindingData.optionsText]
+                        };
+                        r.push( tmpItem );
+                    } );
+                    return r;
+                };
+                bindingData.data = prepareData();
+                console.log( "++++ INIT SELECT2 ++++", bindingData, window.$(el) );
+
+                window.$(el).select2(bindingData);
+
                 ko.utils.domNodeDisposal.addDisposeCallback(el, function () {
-                    $(el).select2('destroy');
+                    window.$(el).select2('destroy');
                 });
             },
             update: function (el, valueAccessor, allBindingsAccessor, viewModel) {
                 var allBindings = allBindingsAccessor();
-                var select2 = $(el).data("select2");
+                var select2 = window.$(el).data("select2");
                 if ("value" in allBindings) {
                     var newValue = "" + ko.unwrap(allBindings.value);
                     if ((allBindings.select2.multiple || el.multiple) && newValue.constructor !== Array) {
